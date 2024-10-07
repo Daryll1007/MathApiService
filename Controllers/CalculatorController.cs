f807353ae26fad1440e4c0292faa2dce0b86976b
Daryll1007 <dchrist8@mycambrian.ca>
2024-10-07 19:06:10 -0400

    Initial commit - Project setup and folder structure

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,48 @@
+﻿using MathApiService.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathApiService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ICalculatorService<double> _calculatorService;
+
+        public CalculatorController(ICalculatorService<double> calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
+        [HttpGet("add")]
+        public ActionResult<double> Add([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok(_calculatorService.Add(a, b));
+        }
+
+        [HttpGet("subtract")]
+        public ActionResult<double> Subtract([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok(_calculatorService.Subtract(a, b));
+        }
+
+        [HttpGet("multiply")]
+        public ActionResult<double> Multiply([FromQuery] double a, [FromQuery] double b)
+        {
+            return Ok(_calculatorService.Multiply(a, b));
+        }
+
+        [HttpGet("divide")]
+        public ActionResult<double> Divide([FromQuery] double a, [FromQuery] double b)
+        {
+            try
+            {
+                return Ok(_calculatorService.Divide(a, b));
+            }
+            catch (DivideByZeroException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Services/CalculatorService.cs
@@ -0,0 +1,29 @@
+﻿using MathApiService.Interfaces;
+
+namespace MathApiService.Services
+{
+    public class CalculatorService : ICalculatorService<double>
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            return a / b;
+        }
+    }
+}
